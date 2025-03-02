a1fda668f1f82c568c9a2735fe315f904fde3dfb
Finjohns <kelvinonginjo08@gmail.com>
2025-03-03 01:47:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "enter your session",
-    CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
+    CAPTION: process.env.CAPTION || "POWERED BY JINWIILONGINJO",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
@@ -17,7 +17,7 @@ module.exports = {
     ANTICALL: process.env.ANTICALL || "true",
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
     HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "254748387615",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254769365617",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
