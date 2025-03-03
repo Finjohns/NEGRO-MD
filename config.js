de4f6d3e55b556b71b0a69f5b9808e0ae7d7716c
Finjohns <kelvinonginjo08@gmail.com>
2025-03-03 16:32:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "254769365617",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r6qxz0.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M NEGRO-MD WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
@@ -29,7 +29,7 @@ module.exports = {
     HEART_REACT: process.env.HEART_REACT || "true",
     CHAT_BOT: process.env.CHAT_BOT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+    BOT_NAME: process.env.BOT_NAME || " 𝐉𝚰𝚴𝐖𝚰𝚰𝐋 𝚯𝚴𝐆𝚰𝚴𝐉𝚯 🕷️🕸️",
     STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү negro-м∂`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
